3cd0c82463ed5d8ca7f45250b6b726ce5e26b8b6
Ankit Anurag <xyzjoker14@gmail.com>
2019-06-20 17:37:54 +0530

    Chapter-3

--- main.js
+++ main.js
@@ -8,8 +8,19 @@ console.log('HELLO WORLD');
 
 //Exercise 2
 
+/*
 var sum = 0;
 for (let index = 2; index < process.argv.length; index++) {
   sum += Number(process.argv[index])
 }
 console.log(sum);
+*/
+
+/*  ********************************************* */
+
+//Exercise 3
+
+var fs = require('fs');
+var buf = fs.readFileSync(process.argv[2]);
+var res = buf.toString().split('\n').length - 1;
+console.log(res);
