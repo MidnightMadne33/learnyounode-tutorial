56053de76a90f6e9b73ef2ee2c417418dc7920f6
Ankit Anurag <xyzjoker14@gmail.com>
2019-06-20 17:30:00 +0530

    lesson-2

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+//Exercise 1
+
+/*
 console.log('HELLO WORLD');
+*/
+
+/*  ********************************************* */
+
+//Exercise 2
+
+var sum = 0;
+for (let index = 2; index < process.argv.length; index++) {
+  sum += Number(process.argv[index])
+}
+console.log(sum);
